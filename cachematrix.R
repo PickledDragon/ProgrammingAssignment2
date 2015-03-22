393b5cef3bd9e312f24e6b8a425a1566efe95033
PickledDragon <praveenkottarathil@gmail.com>
2015-03-23 00:18:49 +0400

    Added additional remarks for easy understanding

--- cachematrix.R
+++ cachematrix.R
@@ -5,40 +5,56 @@
 #' @return A matrix with cache capability
 makeCacheMatrix <- function(x = matrix()) {
   
-  mxInstance <- NULL
+  mxCache <- NULL
   
+  
+  #Generic getter/setter methods for the enhanced Matrix type
   set <- function(paramMatrix)
   {
     x<<-paramMatrix
-    mxInstance<<- NULL
+    mxCache<<- NULL
   }
   
   get <- function() x
   
-  setinverse<- function(inverse) { mxInstance <<- inverse 
+  
+  #Set inverse and get inverse functions to facilitate access to inverse functionality in the extended Matrix type
+  setinverse<- function(inverse) { mxCache <<- inverse 
                                    message("Inverse added in cache")
   }
-  getinverse<- function() mxInstance
+  getinverse<- function() mxCache
   
+  #Exposes methods to the external world
   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Enhances Solver function with cache capability. 
-
+## Enhances Solver function with cache capability
+#' Extends 'solve' function with cache capability
+#' @param x A matrix created with 'makeCacheMatrix' function. Please note primitive matrices will not work with thos function
+#' @return Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  mxInstance<-x$getinverse()
   
-  if(!is.null(mxInstance))
+  #Attempt retrieval of inverse from the matrix object
+  mxCache<-x$getinverse()
+  
+  #check if we're succesful in retrieving the inverse
+  if(!is.null(mxCache))
   {
+    #Found the cached inverse. Return the inverse
     message("Cache hit - return from cache")
-    return(mxInstance)
+    return(mxCache)
     
   }
   
+  #We're hit by a cache miss. Needs to initialize cache
+  #--
+  #Retrieve original matrix from the extended matrix parameter
   srcMat <- x$get()
-  mxInstance<-solve(srcMat) #Solver function will generate inverse of the MX
-  x$setinverse(mxInstance) #Push the result to cache
-  return (mxInstance)
+  
+  #Generate inverse of the matrix - with solve funcion
+  mxCache<-solve(srcMat) #Solver function will generate inverse of the MX
+  
+  x$setinverse(mxCache) #Push the result to cache
+  return (mxCache) # Retrun the inverse
 }
